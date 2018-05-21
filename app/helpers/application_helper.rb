09d1e3ce59802642f7067b5b5677bc5aa00ad640
Ini Akinleye <inioluwaakinleye@gmail.com>
2018-05-21 05:44:53 +0100

    modify helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title(page_title = '')
+  def full_title(page_title)
     base_title = 'Testimony Hostel'
     if page_title.empty?
       base_title + ' | ' + 'Private Hostels'
